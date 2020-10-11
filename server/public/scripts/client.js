129b1cfdbac3c19cbb2b9c86088dfe3a8639a823
Sam Mahler <mahler.sam@gmail.com>
2020-10-10 23:46:08 -0500

    append inputs through GET and POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,50 @@ console.log('js');
 
 $(document).ready(onReady);
 
+let operator;
+
 function onReady() {
     console.log('jquery')
+    // $('#addButton').on('click', sendAdd);
+    // $('#subtractButton').on('click', subtract);
+    // $('#multiplyButton').on('click', multiply);
+    // $('#divideButton').on('click', divide);
+    $('#calculate').on('click', postInputs)
+    getCalc();
 }
+
+function getCalc() {
+    $.ajax({
+        type: 'GET',
+        url: '/calculations'
+    }).then(function(response) {
+        console.log('get response', response);
+        $('#calcList').empty();
+        for (let i = 0; i < response.length; i++) {
+            $('#calcList').append(`
+                <li>${response[i].firstNum} ${response[i].secondNum}</li>
+            `);
+        }
+    }).catch(function(error){
+        alert(error);
+    })
+}
+
+function postInputs() {
+    let firstNum = $('#firstNumIn').val();
+    let secondNum = $('#secondNumIn').val();
+    $.ajax({
+        type: 'POST',
+        url: '/calculations',
+        data: {
+            firstNum: firstNum,
+            secondNum: secondNum
+        }
+    }).then(function(response){
+        getCalc();
+    }).catch(function(error){
+        alert(error);
+    });
+}
+
+
--- server/server.js
+++ server/server.js
@@ -8,6 +8,20 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+let calculationsArray = [];
+
+app.get('/calculations', (req, res) => {
+    res.send(calculationsArray);
+})
+
+
+app.post('/calculations', (req, res) => {
+    calculationsArray.push(req.body);
+    res.sendStatus(200);
+})
+
+
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
